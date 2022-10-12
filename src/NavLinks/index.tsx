4f2f53f6e6cd6178a4a8e6958c239ed3cb500523
robyrty <34003492+robyrty@users.noreply.github.com>
2022-10-12 11:42:43 +0530

    Add files via upload

---
+++ src/NavLinks/index.tsx
@@ -0,0 +1,40 @@
+import * as React from 'react'
+
+import { useNoScroll, computeClass } from '../utils'
+import './styles.scss'
+
+interface NavLinksProps {
+  customClassName: string
+  isOpen: boolean
+  shouldAnimate: boolean
+  leftLinks?: JSX.Element
+  // rightLinks?: JSX.Element
+}
+
+const NavLinks: React.FC<NavLinksProps> = ({
+  customClassName,
+  isOpen,
+  shouldAnimate,
+  leftLinks,
+  // rightLinks
+}) => {
+  const otherClassNames = computeClass(
+    shouldAnimate,
+    isOpen,
+    'animate show',
+    'animate',
+    'nav-links--show'
+  )
+
+  useNoScroll(isOpen)
+
+  return (
+  
+    <div className={`nav-links ${otherClassNames} ${customClassName}`.trim()}>
+      <span className="nav-links__group left">{leftLinks}</span>
+      {/* <span className="nav-links__group right">{rightLinks}</span> */}
+    </div>
+  )
+}
+
+export default NavLinks
